87c0e9685248fb40e1853ab4f403d951291e67ac
Oleksandr Raduka <89737800+rainb00w@users.noreply.github.com>
2022-03-07 17:58:30 +0200

    _v

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,20 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const imageContainer = document.querySelector('.gallery');
+
+console.log(imageContainer);
+
+
+function renderGallery (arrayOfImages) {
+    const markUp = arrayOfImages.map(
+        ({preview, original, description}) => 
+        `<div class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></div>`
+        ).join('');
+
+    imageContainer.insertAdjacentHTML('beforeend', markUp);
+};
+
+renderGallery(galleryItems);
+
