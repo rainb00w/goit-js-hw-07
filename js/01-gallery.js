623ac9cd2fc027d6d4d5ac07c99513915641d5c6
Oleksandr Raduka <89737800+rainb00w@users.noreply.github.com>
2022-03-08 15:46:23 +0200

    _v2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -19,3 +19,36 @@ function renderGallery (arrayOfImages) {
 
 renderGallery(galleryItems);
 
+
+imageContainer.addEventListener('click', onImageClick);
+
+function onImageClick(evt) {
+    evt.preventDefault();
+
+    const imageAddress = evt.target.dataset.source;
+
+    if (!evt.target.classList.contains('gallery__image')){
+        return;
+    }
+    console.log(evt.target);
+
+    const instance = basicLightbox.create(`
+         <img src="${imageAddress}" width="800" height="600">
+     `,
+     {
+        onShow: () => {
+          window.addEventListener("keydown", onEscClick);
+        },
+      }
+     );
+
+     function onEscClick(evt) {
+        if (evt.code === 'Escape') {
+          instance.close();
+          }  
+      }
+
+     instance.show();
+}
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,32 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const imageContainer = document.querySelector('.gallery');
+
+console.log(imageContainer);
+
+
+function renderGallery (arrayOfImages) {
+    const markUp = arrayOfImages.map(
+        ({preview, original, description}) => 
+        `<div class="gallery__item">
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+        </div>`
+        ).join('');
+
+    imageContainer.insertAdjacentHTML('beforeend', markUp);
+};
+
+renderGallery(galleryItems);
+
+
+
+let gallery = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
